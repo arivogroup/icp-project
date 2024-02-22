605ca84ce247de5e42f1a73503929a2d8218b9d0
Miguel Salazar <miguelsalazartomey@gmail.com>
2024-02-22 12:10:02 -0400

    Update Custom Code

--- Plan-data/print.pdf.js
+++ Plan-data/print.pdf.js
@@ -3,25 +3,26 @@ const printButton = document.getElementById("print-icon");
 
 function fillPrintContent() {
   var list_sections = [
-    '-draft-initial-icp', 
-    '-company-size', 
-    '-number-employees', 
-    '-revenue-level', 
-    '-location', 
-    '-age-customer', 
-    '-usual-gender',
-    '-socio-economic',
-    '-educational-level',
-    '-personality',
-    '-lifestyle',
-    '-interests',
-    '-technologies',
-    '-capabilities'
+    "-draft-initial-icp",
+    "-company-size",
+    "-number-employees",
+    "-revenue-level",
+    "-location",
+    "-age-customer",
+    "-usual-gender",
+    "-socio-economic",
+    "-educational-level",
+    "-personality",
+    "-lifestyle",
+    "-interests",
+    "-technologies",
+    "-capabilities",
   ];
-  for (let i = 0; i < list_sections.length; i++ ) {
-    document.getElementById("print" + list_sections[i]).innerText = document.getElementById("content" + list_sections[i]).innerText;
+  for (let i = 0; i < list_sections.length; i++) {
+    document.getElementById("print" + list_sections[i]).innerText =
+      document.getElementById("content" + list_sections[i]).innerText;
   }
-};
+}
 
 // Event DOM completly loaded
 document.addEventListener("DOMContentLoaded", () => {
@@ -30,17 +31,21 @@ document.addEventListener("DOMContentLoaded", () => {
     fillPrintContent();
     element.style.display = "block";
     var opt = {
-      margin: [1, 0 ,1, 0],
-      pagebreak: { before: '.chapterdiv__print', avoid: 'p' },
-      filename: project_name + ' - ' + author_name + '.pdf',
-      image: { type: 'jpg', quality: 0.2 },
+      margin: [1, 0.5, 1, 0.5],
+      pagebreak: { before: ".chapterdiv__print", avoid: ["p", "h3", "h4"] },
+      filename: project_name + " - " + author_name + ".pdf",
+      image: { type: "jpg", quality: 0.2 },
       html2canvas: { allowTaint: true, scale: 1 },
-      jsPDF: { unit: 'cm', format: 'A4', orientation: 'p' }
+      jsPDF: { unit: "cm", format: "A4", orientation: "p" },
     };
-    html2pdf().set(opt).from(element).save().catch(err => console.log(err));
+    html2pdf()
+      .set(opt)
+      .from(element)
+      .save()
+      .catch((err) => console.log(err));
     let waitTimeOutPrint = setTimeout(hidePrintSection, 2000);
     function hidePrintSection() {
       element.style.display = "none";
-    };
+    }
   });
 });
