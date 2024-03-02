c1ca4749c07f22674d54d9521f63ec4a25630165
Miguel Salazar <miguelsalazartomey@gmail.com>
2024-03-02 10:31:30 -0400

    Custom Code Update

--- User-Pages/signup.emailvalidation.js
+++ User-Pages/signup.emailvalidation.js
@@ -2,4 +2,7 @@ const formSubmitButton = document.getElementById('submit-button');
 const errorEmailLabel = document.getElementById('emailServer-Error');
 const emailInput = document.getElementById('wf-sign-up-email');
 
-
+function loadFreeDomains(callback) {
+  const xhr = new XMLHttpRequest();
+  
+}
