88cfdf05107115b469c8e11d60e46632e31a1290
Miguel Salazar <miguelsalazartomey@gmail.com>
2024-03-05 20:18:57 -0400

    Custom Code Update

--- Plan-data/page.init.js
+++ Plan-data/page.init.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
   // next-data Info
+
+  //Determinar Visibilidad o no de Loader, hacer OverFlow:Hidden al body
+  var bodyElement = document.querySelector('.body-home-plandata');
+  var loadingElement = document.querySelector('.loading-chapter');
+
+  function isLoadingDisplayed() {
+    var styleDisplay = getComputedStyle(loadingElement).display;
+    if (styleDisplay === 'flex') {
+      bodyElement.classList.add('overflow-hidden');
+    } else {
+      bodyElement.classList.remove('overflow-hidden');
+    }
+  }
+
+  isLoadingDisplayed();
+  var observer = new MutationObserver(isLoadingDisplayed);
+  observer.observe(loadingElement, { attributes: true, attributeFilter: ['style'] });
+
   var address =
     "https://scale-faster.webflow.io/plan-data-next/" +
     slug_init +
